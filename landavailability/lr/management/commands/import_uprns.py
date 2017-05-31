0c06cefb9986d3b3e8e8595cf7c60401e8e329bf
David Read <david.read@hackneyworkshop.com>
2017-05-31 17:58:01 +0100

    uprn cli import now updated for M2M model.

--- landavailability/lr/management/commands/import_uprns.py
+++ landavailability/lr/management/commands/import_uprns.py
@@ -1,9 +1,24 @@
+import sys
+
 from lr.models import Title, Uprn
 from .importers import CSVImportCommand
 
 
 class Command(CSVImportCommand):
     help = 'Import UPRNs from a CSV file'
+
+    def handle(self, *args, **options):
+        # we need to start from a blank uprn table, otherwise out-of-date
+        # links in the uprn-title m2m will not get deleted.
+        num_uprns = Uprn.objects.count()
+        if num_uprns > 0:
+            response = input('There are {} uprns in the db that need deleting.'
+                             ' Continue? (y/n) >'.format(num_uprns))
+            if response.lower() != 'y':
+                sys.exit()
+            Uprn.objects.all().delete()
+
+        super(Command, self).handle(*args, **options)
 
     def process_row(self, row):
         title_id, uprn_id, add_or_update = row
@@ -12,23 +27,27 @@ class Command(CSVImportCommand):
         except Title.DoesNotExist:
             # print('Cannot add UPRN {0} - missing Title entry: {1}'
             #       .format(uprn_id, title_id))
-            outcome = 'ignored - uprn does not match any Title'
+            outcome = 'ignored - uprn does not match any title in the db'
         else:
             try:
                 uprn = Uprn.objects.get(uprn=uprn_id)
-                if uprn.title == title:
-                    return 'unchanged'
-                outcome = 'updated'
+                outcome = 'added'
             except Uprn.DoesNotExist:
                 uprn = Uprn()
                 uprn.uprn = uprn_id
-                outcome = 'created'
+                try:
+                    uprn.save()
+                except Exception as e:
+                    print('Cannot save: {} - {}'.format(row, e))
+                    return 'Error: could not save new uprn'
 
-            uprn.title = title
+                outcome = 'added (also created uprn)'
+
+            uprn.titles.add(title)
 
             try:
                 uprn.save()
             except Exception as e:
                 print('Cannot add: {} - {}'.format(row, e))
-                outcome = 'Error: could not save object'
+                outcome = 'Error: could not save uprn link'
         return outcome
